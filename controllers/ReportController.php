06bbb05291f39050f37d1d7bd25ebf4d17c86697
Minhdeptry <minhhdeptry@gmail.com>
2025-12-04 12:44:55 +0700

    quản lý báo cáo

---
+++ controllers/ReportController.php
@@ -0,0 +1,24 @@
+<?php
+class ReportController
+{
+    private $model;
+
+    public function __construct()
+    {
+        require_once __DIR__ . "/../models/ReportModel.php";
+        $this->model = new ReportModel();
+    }
+
+    public function index($act)
+    {
+        $year = isset($_GET['year']) ? intval($_GET['year']) : date('Y');
+
+        // Lấy dữ liệu báo cáo
+        $customerData = $this->model->getTotalCustomersByTour($year);
+        $revenueData  = $this->model->getRevenueByTour($year);
+
+        $currentAct = $act;
+        $view = "./views/admin/Report/index.php";
+        include "./views/layout/adminLayout.php";
+    }
+}
